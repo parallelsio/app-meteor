1b72394cedaa3c0c7972c73c4c29d3f90080c9d1
Angel Balcarcel <abalcarc@thoughtworks.com>
2015-01-05 08:37:27 -0500

    hide browser extension test helper link text

--- extensions/chrome/scripts/inject.testHelper.js
+++ extensions/chrome/scripts/inject.testHelper.js
@@ -8,7 +8,7 @@ requirejs([], function () {
     console.log('init:testHelper');
 
     // a tags seem to get blocked on a lot of sites, including our own
-    $( 'body' ).append( '<div id="activate-extension" style="position: absolute; top: 0px;" class="test-helper">Activate Extension</div>' );
+    $( 'body' ).append( '<div id="activate-extension" style="position: absolute !important; top: 0px !important; color: transparent !important; background-color: transparent !important;" class="test-helper">Activate Extension</div>' );
 
     $('#activate-extension').click(function () {
       console.log('testHelper:clipper-activated');
